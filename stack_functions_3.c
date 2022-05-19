115825f1b29c3908042e2b3457752858110c77b4
J3rCast <jerson.castro.r@gmail.com>
2022-05-19 17:46:14 -0500

    statements added

--- stack_functions_3.c
+++ stack_functions_3.c
@@ -13,7 +13,7 @@ void pchar(stack_t **top, __attribute__((unused))unsigned int n)
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", lineNum);
 		exit(EXIT_FAILURE);
 	}
-	if ((*top)->n > 127)
+	if (isprint((*top)->n) != 0)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", lineNum);
 		exit(EXIT_FAILURE);
