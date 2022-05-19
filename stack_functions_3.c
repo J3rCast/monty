cfb12692f4d7ff6a8d21db00d82e8d4d6b4df1a0
J3rCast <jerson.castro.r@gmail.com>
2022-05-19 17:35:42 -0500

    statements added

--- stack_functions_3.c
+++ stack_functions_3.c
@@ -8,6 +8,16 @@ void pchar(stack_t **top, __attribute__((unused))unsigned int n)
 {
 	char character = 'a';
 
+	if (*top == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", lineNum);
+		exit(EXIT_FAILURE);
+	}
+	if ((*top)->n > 256)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", lineNum);
+		exit(EXIT_FAILURE);
+	}
 	character = (*top)->n;
 
 	printf("%c\n", character);
